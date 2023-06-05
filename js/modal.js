1cc40a64c64a80313eb34e1c0e14a3b419d2ec6b
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-06-05 11:24:24 +0900

    qeustion Modal 기능 html 및 Css 추가

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,66 @@
-//필요한 엘리먼트들을 선택한다.
-const openButton = document.querySelector(".md_open");
-const modal = document.querySelector(".modal");
-const overlay = modal.querySelector(".md_overlay");
-const closeButton = modal.querySelector(".md_close");
 
-//동작함수
+const $openButton = document.querySelector(".md_open");
+const $modal = document.querySelector(".modal");
+const $closeButton = $modal.querySelector(".md_close");
+const $showSearch = $modal.querySelector(".show_search");
+const $showDisplay = $modal.querySelector(".show_display");
+const $showVideo = $modal.querySelector(".show_video");
+const $searchDiv = $modal.querySelector(".explanation_Search")
+const $displayDiv = $modal.querySelector(".explanation_Display")
+const $videoDiv = $modal.querySelector(".explanation_Video")
+
 const handleModal = (e) => {
     e.preventDefault();
-    modal.classList.toggle("hidden");
-    const body = document.querySelector('body')
+    $modal.classList.toggle("hidden");
 };
 
-//클릭 이벤트
-openButton.addEventListener('click',handleModal)
-closeButton.addEventListener("click", handleModal);
+const handleShowSearch = () => {
+    $showDisplay.classList.remove('bg-blue-400')
+    $showDisplay.classList.add('bg-gray-400')
+    
+    $showSearch.classList.remove('bg-gray-400')
+    $showSearch.classList.add('bg-blue-400')
+
+    $showVideo.classList.remove('bg-blue-400')
+    $showVideo.classList.add('bg-gray-400')
+
+    $searchDiv.classList.remove('hidden')
+    $videoDiv.classList.add('hidden')
+    $displayDiv.classList.add('hidden')
+}
+
+const handleShowDisplay = () => {
+    $showVideo.classList.remove('bg-blue-400')
+    $showVideo.classList.add('bg-gray-400')
+
+    $showSearch.classList.remove('bg-blue-400')
+    $showSearch.classList.add('bg-gray-400')
+
+    $showDisplay.classList.remove('bg-gray-400')
+    $showDisplay.classList.add('bg-blue-400')
+
+    $searchDiv.classList.add('hidden')
+    $videoDiv.classList.add('hidden')
+    $displayDiv.classList.remove('hidden')
+}
+
+const handleShowVideo = () => {
+    $showDisplay.classList.remove('bg-blue-400')
+    $showDisplay.classList.add('bg-gray-400')
+    
+    $showSearch.classList.remove('bg-blue-400')
+    $showSearch.classList.add('bg-gray-400')
+
+    $showVideo.classList.remove('bg-gray-400')
+    $showVideo.classList.add('bg-blue-400')
+
+    $searchDiv.classList.add('hidden')
+    $videoDiv.classList.remove('hidden')
+    $displayDiv.classList.add('hidden')
+}
+
+$openButton.addEventListener("click",handleModal)
+$closeButton.addEventListener("click", handleModal);
+$showSearch.addEventListener("click",handleShowSearch)
+$showDisplay.addEventListener("click",handleShowDisplay)
+$showVideo.addEventListener("click",handleShowVideo)
